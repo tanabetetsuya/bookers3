8b68e15f4fa0adde82861102a95ce04ac7eb8156
tanabetetsuya <tetsumagic123@gmail.com>
2023-06-17 03:17:48 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,8 +16,27 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book), notice: 'Book was successfully created!'
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: 'Book was successfully Updated'
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
     redirect_to books_path
   end
   
